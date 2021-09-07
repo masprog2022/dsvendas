b1c89d9c658ee66d7961de950e8a2b90d22d60a7
masprog2022 <masprog2022@gmail.com>
2021-09-07 10:30:25 +0100

    update taxa de sucesso

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         <h1 className="text-primary py-3">Dashboard de Vendas</h1>
         <div className="row px-3">
           <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Taxa de sucesso </h5>
+            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
             <BarChart />
           </div>
           <div className="col-sm-6">
@@ -24,7 +24,7 @@ function App() {
         <div className="py-3">
           <h2 className="text-primary">Todas Vendas</h2>
         </div>
-        <DataTable />(%)
+        <DataTable />
       </div>
       <Footer />
     </>
